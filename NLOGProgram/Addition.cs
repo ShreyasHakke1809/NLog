c8e14184c6dd115260ac1e8a016f7f01e484cae6
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-19 19:22:25 +0530

    Adding nlog file with program

---
+++ NLOGProgram/Addition.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLOGProgram
+{
+    internal class Addition
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public static void SumOfTwoNumbers(int firstnum, int secondnum)
+        {
+            int sum = 0;
+            if (firstnum <= 0 || secondnum <= 0)
+            {
+                logger.Warn("Input should be greater than zero");
+                logger.Error("Give proper +ve inputs");
+            }
+            else
+            {
+                sum = firstnum * secondnum;
+                logger.Info("Succesfully added the numbers and sum is :{0}" + sum);
+            }
+        }
+    }
+}
---
+++ NLOGProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NLOGProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a first number");
+            int firstnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number");
+            int secondnum = Convert.ToInt32(Console.ReadLine());
+            Addition.SumOfTwoNumbers(firstnum, secondnum);
+        }
+    }
+}
